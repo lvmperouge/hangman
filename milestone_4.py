a762a62dd3c242b36465bff4f3b8fb13cf151bee
Daria <lvmperouge@gmail.com>
2023-10-21 00:26:09 +0100

    Fixes typo.

--- milestone_4.py
+++ milestone_4.py
@@ -2,11 +2,11 @@ import random
 word_list = ["mango", "grape", "coconut", "strawberry", "tomato"]
 class Hangman():
     def __init__(self, word_list, num_lives=5):
+        self.word_list=word_list
         self.word=random.choice(word_list)
         self.word_guessed=["_"]*len(self.word)
         self.num_letters=0
         self.num_lives=num_lives
-        self.word_list=word_list
         self.list_of_guesses=[]
     
     def check_guess(guess):
