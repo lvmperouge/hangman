f3ac9594a546c043625d345919b7b5a15ed55cc7
Daria <lvmperouge@gmail.com>
2023-10-20 20:36:00 +0100

    fixed typo

--- milestone_3.py
+++ milestone_3.py
@@ -1,10 +1,4 @@
-def check_guess(letter):
-    letter = letter.lower()    
-    if letter in word: 
-        print("Good guess, ", guess, "is in the word!")
-    else: 
-        print("Sorry,", guess, "is not in the word. Try again.")
-
+word={}
 def ask_for_input():
     while True:
         guess = str(input("Guess a letter!"))
@@ -13,6 +7,12 @@ def ask_for_input():
         else:
             print("Invalid letter. Please, enter a single alphabetical character.")
 
+def check_guess():
+    guess = guess.lower()    
+    if guess in word: 
+        print("Good guess, ", guess, "is in the word!")
+    else: 
+        print("Sorry,", guess, "is not in the word. Try again.")
 
 
 
