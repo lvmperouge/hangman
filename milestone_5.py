a0e3e902ade560bb8109b50a887318abbc9f5ca9
Daria <lvmperouge@gmail.com>
2023-10-21 16:53:56 +0100

    Adds final fixes.

--- milestone_5.py
+++ milestone_5.py
@@ -41,13 +41,14 @@ def play_game(word_list):
     num_lives=5
     game = Hangman(word_list, num_lives)
     while True:
-        if num_lives==0:
+        if game.num_lives==0:
             print ("You lost!")
             break
         if game.num_letters>0: 
             game.ask_for_input()
+            print(game.word_guessed)
             continue
-        if num_lives!=0 and game.num_letters==0:
+        if game.num_lives!=0 and game.num_letters==0:
             print("Congratulations. You won the game!")
             break
 play_game(word_list)
